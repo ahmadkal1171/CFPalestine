902946fa4cada9da992af32559cbe41a7a6a4938
Smiling <106730008+snfgnek@users.noreply.github.com>
2024-01-14 00:23:00 +0800

    Update LoginUserDao.java

--- src/java/dao/LoginUserDao.java
+++ src/java/dao/LoginUserDao.java
@@ -9,7 +9,7 @@ import util.DBConnection;
 
 public class LoginUserDao {
     public String authenticateUser(LoginUser loginUser){
-        String fundername=loginUser.getFundername();
+        String funderusername=loginUser.getFunderusername();
         String funderpass=loginUser.getFunderpass();
         
         Connection conn=null;
@@ -21,12 +21,12 @@ public class LoginUserDao {
         try{
             conn=DBConnection.createConnection();
             statement=conn.createStatement();
-            resultSet=statement.executeQuery("SELECT FUNDERNAME,FUNDERPASS FROM FUNDER");
+            resultSet=statement.executeQuery("SELECT funderusername,funderpass FROM funders");
             while(resultSet.next()){
-                usernameDB=resultSet.getString("funderName");
-                passwordDB=resultSet.getString("funderPass");
+                usernameDB=resultSet.getString("funderusername");
+                passwordDB=resultSet.getString("funderpass");
                 
-                if(fundername.equals(usernameDB)&&funderpass.equals(passwordDB)){
+                if(funderusername.equals(usernameDB)&&funderpass.equals(passwordDB)){
                     return "SUCCESS";
                 }
             }  
